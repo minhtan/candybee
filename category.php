5fedbf07248829fce5dc027606e43a4b4ebf4514
Tan <m.tan2007@gmail.com>
2014-09-09 16:19:36 +0700

    move header to primary-wrap

--- category.php
+++ category.php
@@ -11,44 +11,41 @@
 
 get_header(); ?>
 
-	<section id="primary-cat" class="content-area">
-		<div class="primary-swap">
-			<div id="content" class="site-content" role="main">
-				<?php if ( have_posts() ) : ?>
-					<ul class="ul-cat-nav">
-						<?php while ( have_posts() ) : the_post(); ?>
-							<li class="listing-cat row">
-								<div class="col-4 size-img">
+		<div id="content" class="site-content" role="main">
+			<?php if ( have_posts() ) : ?>
+				<ul class="ul-cat-nav">
+					<?php while ( have_posts() ) : the_post(); ?>
+						<li class="listing-cat row">
+							<div class="col-4 size-img">
+								<a href="<?php echo get_permalink();  ?>">
+									<img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id()); ?>"/>
+								</a>		
+							</div>
+							<div class="post-cat col-8">
+								<h2 class="title-post-cat">
 									<a href="<?php echo get_permalink();  ?>">
-										<img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id()); ?>"/>
-									</a>		
-								</div>
-								<div class="post-cat col-8">
-									<h2 class="title-post-cat">
-										<a href="<?php echo get_permalink();  ?>">
-											<?php echo the_title(); ?>
-										</a>
-									</h2>
-									<p>
-										<?php echo get_the_short_excerpt(); ?>
-									</p>
-									<a class="next-view" href="<?php echo get_permalink(); ?>">
-										xem tiếp
+										<?php echo the_title(); ?>
 									</a>
-								</div>
-							</li>
-						<?php endwhile; ?>
-					</ul>
-					<?php
-					    candybee_paging_nav();
-					?>
-				<?php else :
-					get_template_part( 'content', 'none' );
+								</h2>
+								<p>
+									<?php echo get_the_short_excerpt(); ?>
+								</p>
+								<a class="next-view" href="<?php echo get_permalink(); ?>">
+									xem tiếp
+								</a>
+							</div>
+						</li>
+					<?php endwhile; ?>
+				</ul>
+				<?php
+				    candybee_paging_nav();
 				?>
-				<?php endif; ?>
+			<?php else :
+				get_template_part( 'content', 'none' );
+			?>
+			<?php endif; ?>
 
-			</div><!-- #content -->
-		</div>
-	</section><!-- #primary -->
+		</div><!-- #content -->
+	</div><!-- #primary-wrap -->
 
 <?php get_footer();?>
--- single.php
+++ single.php
@@ -6,24 +6,23 @@
  */
 
 get_header(); ?>
-	<div class="cat_list_wrap">
-		<ul class="cat_list">
-			<li class="one_cat"><a class="cat_link" data-hover="Home" href="<?php echo home_url();?>">Home</a></li>
-			<?php
-			$args = array(
-			  'orderby' => 'name'
-			  );
-			$categories = get_categories( $args );
-			foreach ( $categories as $category ) : ?>
-				<?php 
-					$cat_link =  get_category_link($category->term_id);
-					$cat_name = $category->name;
-			 	?>
-				<li class="one_cat"><a class="cat_link" data-hover="<?php echo $cat_name; ?>" href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a></li>
-			<?php endforeach; ?>
-		</ul>
-	</div>
-	<div class="primary-wrap">
+		<div class="cat_list_wrap">
+			<ul class="cat_list">
+				<li class="one_cat"><a class="cat_link" data-hover="Home" href="<?php echo home_url();?>">Home</a></li>
+				<?php
+				$args = array(
+				  'orderby' => 'name'
+				  );
+				$categories = get_categories( $args );
+				foreach ( $categories as $category ) : ?>
+					<?php 
+						$cat_link =  get_category_link($category->term_id);
+						$cat_name = $category->name;
+				 	?>
+					<li class="one_cat"><a class="cat_link" data-hover="<?php echo $cat_name; ?>" href="<?php echo $cat_link; ?>"><?php echo $cat_name; ?></a></li>
+				<?php endforeach; ?>
+			</ul>
+		</div>
 		<div id="primary" class="content-area row">
 			<main id="main" class="site-main col-9" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -39,5 +38,5 @@ get_header(); ?>
 				</div>
 			</div>	
 		</div><!-- #primary -->
-	</div>
+	</div><!-- #primary-wrap -->
 <?php get_footer(); ?>
